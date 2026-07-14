5bd9f1ba60b786f51b9431fd4e1013d83e8d957c
sh969bolt <sh969@ya.ru>
2026-07-15 01:24:24 +0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // ИСПРАВЛЕНО: Импортируем HashRouter вместо BrowserRouter
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter> {/* ИСПРАВЛЕНО: Оборачиваем приложение в HashRouter */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
